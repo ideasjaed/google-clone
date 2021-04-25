31ad5b6a5cdbea64278f594db155e85cc511da31
Edgar Alberto <edgar.alvarez@suramexico.com>
2021-04-25 01:49:48 -0500

    change to spanish

--- keys.js
+++ keys.js
@@ -3,5 +3,5 @@ export const API_KEY = "AIzaSyCNuZwtgyVZKYzqmrvRxuJt8WBgxR-oBQM";
 //export const API_KEY = "ksdj23ljkkfsjhdkjdvhjk3jh3k4hjkh";
 
 //context key
-export const CONTEXT_KEY = "52756801a364706dd";
+export const CONTEXT_KEY = "55ed9c7842e31ad0b";
 //export const CONTEXT_KEY = "9867c733a42c0c643";
